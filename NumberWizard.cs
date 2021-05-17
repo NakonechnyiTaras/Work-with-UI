054093d10e4763cd0d396e32cdafeef43637622c
NakonechnyiTaras <taras502050@gmail.com>
2021-05-17 18:32:29 +0200

    course from Udemy

---
+++ NumberWizard.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+
+   
+
+    void StartGame()
+    {
+
+        OnNextGuess();
+      
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        OnNextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        OnNextGuess();
+    }
+
+    public void OnNextGuess()
+    {
+       
+        guess = Random.Range(min, max );
+
+        if(guess > 1000)
+        {
+            guess = 1000;
+        }
+
+        guessText.text = guess.ToString();
+
+    }
+
+    
+}
